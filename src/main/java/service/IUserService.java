22a93f2ec8b04192fdfff97c05639de047cccfcb
Mogmi <mogmi95@gmail.com>
2012-07-06 03:05:18 +0200

    Implemented webservice for Users.

--- src/main/java/service/IUserService.java
+++ src/main/java/service/IUserService.java
@@ -5,9 +5,11 @@
 package service;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -15,13 +17,42 @@ import javax.ws.rs.Produces;
  * @author dev
  */
 @WebService
-@Path("/user")
+@Path("/")
 public interface IUserService {
     
     @WebMethod(action = "add")
     @GET
     @Path("add/{login}/{password}")
     @Produces("application/json")
-    public String add();
+    public String add(@PathParam("login") @WebParam(name = "login") String login,
+        @PathParam("password") @WebParam(name = "password") String password);
+
+    @WebMethod(action = "delete")
+    @GET
+    @Path("/delete/{login}")
+    @Produces("application/json")
+    public String delete(@PathParam("login") @WebParam(name = "login") String login);
+    
+    @WebMethod(action = "update")
+    @GET
+    @Path("/update/{oldlogin}/{login}/{password}/{isadmin}")
+    @Produces("application/json")
+    public String update(@PathParam("oldlogin") @WebParam(name = "oldlogin") String oldlogin,
+            @PathParam("login") @WebParam(name = "login") String login,
+            @PathParam("password") @WebParam(name = "password") String password,
+            @PathParam("isadmin") @WebParam(name = "isadmin") Boolean isadmin);
+
+    @WebMethod(action = "findAll")
+    @GET
+    @Path("/findAll")
+    @Produces("application/json")
+    public String findAll();
+    
+    @WebMethod(action = "searchByLogin")
+    @GET
+    @Path("/searchByLogin/{login}")
+    @Produces("application/json")
+    public String searchByLogin(@PathParam("login") @WebParam(name = "login") String login);
+
 }
 
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,22 +4,86 @@
  */
 package service;
 
+import dao.IApplicationDAO;
+import dao.IUserDAO;
+import entity.User;
+import java.io.IOException;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author dev
  */
-@WebService(serviceName = "UserService")
-public class UserService {
+@WebService(endpointInterface = "service.IUserService")
+public class UserService implements IUserService{
 
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @Autowired
+    IUserDAO dao;
+    private ObjectMapper mapper = new ObjectMapper();
+    
+    @Override
+    public String add(String login, String password) {
+        User a = new User();
+        a.setLogin(login);
+        a.setPassword(password);
+        
+        if (dao.add(a)) {
+            return "{ \"status\":\"OK\" }";
+        } else {
+             return "{ \"status\":\"KO\" }";
+        }
     }
+
+    @Override
+    public String delete(String login) {
+        User a = dao.get(login);
+
+        if (a != null && dao.remove(a)) {
+            return "{ \"status\":\"OK\" }";
+        } else {
+            return "{ \"status\":\"KO\" }";
+        }
+    }
+
+    @Override
+    public String update(String oldlogin, String login, String password, Boolean isadmin) {
+        User a = new User();
+
+        a.setLogin(login);
+        a.setPassword(password);
+        a.setAdmin(isadmin);
+        if (dao.update(oldlogin, a)) {
+            return "{ \"status\":\"OK\" }";
+        } else {
+            return "{ \"status\":\"KO\" }";
+        }
+    }
+
+    @Override
+    public String findAll() {
+        List<User> l = dao.findAll();
+
+        try {
+            return mapper.writeValueAsString(l);
+        } catch (IOException ex) {
+            return "{ \"status\":\"KO\" }";
+        }
+    }
+
+    @Override
+    public String searchByLogin(String login) {
+        List<User> l = dao.searchByLogin(login);
+
+        try {
+            return mapper.writeValueAsString(l);
+        } catch (IOException ex) {
+            return "{ \"status\":\"KO\" }";
+        }
+    }
+
 }
